3535c92ee89c1cb85fb8f3e0e1601b6abb844ae0
giovannipessiva <rpt@altervista.org>
2019-02-28 18:28:21 +0100

    scheletro main

--- src/main/java/it/reply/challenge/fantabosco/hashcode/Main.java
+++ src/main/java/it/reply/challenge/fantabosco/hashcode/Main.java
@@ -1,5 +1,7 @@
 package it.reply.challenge.fantabosco.hashcode;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.List;
 
 import utils.FileUtils;
@@ -17,15 +19,38 @@ public class Main {
 		String dataset = DATASET_A;
 		List<String> file = FileUtils.readFile("challenge/" + dataset);
 		
+		
 		// Parser
 		//TODO
+		
 		
 		// Solver
 		//TODO
 		
+		
 		// Serializer & validator
+		StringBuilder solution = new StringBuilder();
 		//TODO
 		
+		
+		// Writer
+		BufferedWriter bufferedWriter = null;
+		FileWriter fileWriter = null;
+		try {
+			String outputFile = dataset.replace(".in", ".out");
+			fileWriter = new FileWriter(outputFile);
+			bufferedWriter = new BufferedWriter(fileWriter);
+			bufferedWriter.append(solution);
+			System.out.println("Soluzion wrote to: " + outputFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				bufferedWriter.close();
+				fileWriter.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
